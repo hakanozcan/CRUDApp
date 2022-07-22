6c2ae1aad9e6719925d9781747738da6572e7f03
Hakan Özcan <hakanozcan20@hotmail.com.tr>
2022-07-23 02:55:19 +0300

    migration added

---
+++ CRUDApp.API/CRUDApp.API/Data/CrudAppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUDApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDApp.API.Data
+{
+    public class CrudAppDbContext:DbContext
+    {
+        public CrudAppDbContext(DbContextOptions options):base(options)
+        {
+
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ CRUDApp.API/CRUDApp.API/Migrations/20220722235219_Initial migration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUDApp.API.Migrations
+{
+    public partial class Initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Phone = table.Column<long>(nullable: false),
+                    Salary = table.Column<long>(nullable: false),
+                    Department = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
