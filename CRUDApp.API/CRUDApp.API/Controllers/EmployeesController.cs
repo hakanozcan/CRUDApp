7669ad88c5f141f6d4c46f5cd3c425bdd204ca01
Hakan Özcan <hakanozcan20@hotmail.com.tr>
2022-07-24 15:51:39 +0300

    project finished

--- CRUDApp.API/CRUDApp.API/Controllers/EmployeesController.cs
+++ CRUDApp.API/CRUDApp.API/Controllers/EmployeesController.cs
@@ -36,5 +36,58 @@ namespace CRUDApp.API.Controllers
 
             return Ok(employeeRequest);
         }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetEmployee([FromRoute]Guid id)
+        {
+            var employee=
+                await _crudAppDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            if (employee==null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute]Guid id,Employee updateEmployeeRequest)
+        {
+             var employee= await _crudAppDbContext.Employees.FindAsync(id);
+
+            if (employee==null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = updateEmployeeRequest.Name;
+            employee.Email = updateEmployeeRequest.Email;
+            employee.Salary = updateEmployeeRequest.Salary;
+            employee.Phone = updateEmployeeRequest.Phone;
+            employee.Department = updateEmployeeRequest.Department;
+
+            await _crudAppDbContext.SaveChangesAsync();
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id)
+        {
+            var employee = await _crudAppDbContext.Employees.FindAsync(id);
+
+            if (employee==null)
+            {
+                return NotFound();
+            }
+
+            _crudAppDbContext.Employees.Remove(employee);
+            await _crudAppDbContext.SaveChangesAsync();
+            return Ok();
+        }
+
     }
 }
--- CRUDApp.UI/src/app/services/employees.service.ts
+++ CRUDApp.UI/src/app/services/employees.service.ts
@@ -24,4 +24,18 @@ export class EmployeesService {
     return this.http.post<Employee>(this.baseApiUrl + '/api/Employees', addEmployeeRequest);
    
   }
+
+  getEmployee(id:string):Observable<Employee>{
+     return this.http.get<Employee>(this.baseApiUrl + '/api/Employees/'+id);
+  }
+
+
+  updateEmployee(id:string,updateEmployeeRequest:Employee):Observable<Employee>{
+    return this.http.put<Employee>(this.baseApiUrl + '/api/Employees/'+id,updateEmployeeRequest);
+  } 
+
+  deleteEmployee(id:string):Observable<Employee>{
+    return this.http.delete<Employee>(this.baseApiUrl + '/api/Employees/'+id);
+  }
+
 }
